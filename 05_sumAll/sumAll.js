b1afc88432a33c9b0188750933765914aa0c9d90
Thomas Harrison <thomasjh@gmail.com>
2024-01-08 12:27:43 +0100

    next exercise complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (a < 0 || b < 0 || Number.isInteger(a) == false || Number.isInteger(b) == false){
+        return "ERROR";
+    }
+        else {
+            if (a < b) {
+                var sum = 0;
+                while (a <= b) {
+                    sum = sum + a;
+                    a++;
+                }
+                return sum;
+                }
+            
+            else if (b < a) {
+                var sum = 0;
+                while (b <= a) {
+                    sum = sum + b;
+                    b++;
+                }
+                return sum;
+            }
+            else {
+                return "ERROR";
+            }
+        }
 };
 
 // Do not edit below this line
