25cc7d1b8d28b5a1b60e8ed92e012e645c9794c8
Thomas Harrison <thomasjh@gmail.com>
2024-01-08 12:04:55 +0100

    next exercise complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,25 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...theArgs) {
+
+    let originalArray = array;
+
+    if (theArgs.length > 1){
+        for (const arg of theArgs) {
+            if (originalArray.includes(arg)) {
+                let position = originalArray.indexOf(arg);
+                originalArray.splice(position, 1);
+            }        
+    }}
+    else {
+        if (originalArray.includes(theArgs[0])) {
+        let singlePosition = originalArray.indexOf(theArgs[0]);
+        originalArray.splice(singlePosition, 1);}
+    }
+
+    return originalArray;
 
 };
+//You can simplify by removing the bottom else statement and changing the first if statement to >= 1.
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
