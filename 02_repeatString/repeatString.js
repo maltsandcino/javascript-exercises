85216f5090ba5ef7dbaea7367558ec9cf1e60c00
Thomas Harrison <thomasjh@gmail.com>
2024-01-08 10:40:22 +0100

    first two exercises complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,21 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+
+    let word = string;
+    let times = num;
+
+    if (num === 0){
+        return "";
+    }
+    else if (num < 0){
+        return "ERROR";
+    }
+    else{
+    for(i = 0; i < times - 1; i++){
+        word = word + string;
+    }
+
+    return word;
+}
 
 };
 
