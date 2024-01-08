d2950959ea0bdc37a4ec2cfe844a19e63f9fa888
Thomas Harrison <thomasjh@gmail.com>
2024-01-08 12:35:30 +0100

    Next exercise complete

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,17 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+    let leap;
+    if (year % 100 == 0 && year % 400 == 0){
+        leap = true;
+    }
+    else if (year % 4 == 0 && year % 100 != 0){
+        leap = true;
+    }
+    else {
+        leap = false;
+    }
+
+    return leap;
 
 };
 
