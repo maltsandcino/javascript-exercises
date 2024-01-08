85d38d076aee8d7b9fc129840c7ee65f4e8b1d81
Thomas Harrison <thomasjh@gmail.com>
2024-01-08 12:45:17 +0100

    completed last exercise for this lesson

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let newTemp = (temp - 32) / 1.8;
+  return +newTemp.toFixed(1)
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let newTemp = (temp * 1.8) + 32;
+  return +newTemp.toFixed(1)
 };
 
 // Do not edit below this line
